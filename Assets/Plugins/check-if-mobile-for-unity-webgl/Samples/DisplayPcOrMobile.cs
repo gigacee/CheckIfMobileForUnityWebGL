6d31042e880174698d4a46ac29418ac24545eff4
Gigacee <gigacee@gigacreation.jp>
2020-10-10 11:04:34 +0900

    Add plugin and sample

---
+++ Assets/Plugins/check-if-mobile-for-unity-webgl/Samples/DisplayPcOrMobile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CheckIfMobileForUnityWebgl.Samples
+{
+    [RequireComponent(typeof(Text))]
+    public class DisplayPcOrMobile : MonoBehaviour
+    {
+        bool isMobile;
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+        [System.Runtime.InteropServices.DllImport("__Internal")]
+        static extern bool IsMobile();
+#endif
+
+        void Start()
+        {
+#if !UNITY_EDITOR && UNITY_WEBGL
+            isMobile = IsMobile();
+#endif
+
+            GetComponent<Text>().text = isMobile ? "Mobile" : "PC";
+        }
+    }
+}
